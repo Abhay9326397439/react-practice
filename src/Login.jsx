d5009f2b2e48739c7f3df3e711f5f9f2430baf02
Abhay9326397439 <abhayzombade26@gmail.com>
2025-12-28 19:23:35 +0530

    created login component

---
+++ src/Login.jsx
@@ -0,0 +1,13 @@
+function Login(){
+    return (
+        <div>
+            <form name="login">
+                Username:<input type="text" placeholder="Enter username"></input><br></br>
+                Password:<input type="password" placeholder="Enter password"></input><br></br>
+                <button>Login here</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
