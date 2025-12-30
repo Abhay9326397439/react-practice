0954e82432dff70d03313f87cedf894aa6ceea10
Abhay9326397439 <abhayzombade26@gmail.com>
2025-12-30 23:34:17 +0530

    rendering array

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,10 +1,10 @@
 import "./ProductItem.css";
 
-function ProductItem({title, price}) {
+function ProductItem({title, price, features}) {
     return (
         <div>
         <h2 className="product-title">This is a {title} and this is that product price is={price}</h2>
-
+        <h3 className="product-features">Features: {features.color}</h3>
         </div>
     );
 
