611d153919950dbc6f6b1983e7d81dcfa1eccbae
Abhay9326397439 <abhayzombade26@gmail.com>
2025-12-30 23:32:47 +0530

    adding array

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,9 +1,10 @@
 import ProductItem from "./ProductItem";
 
 function Product(){
+    let options = [{color:"red"}, {size:"M"}, {brand:"xyz"}];
     return (
         <div>
-      <ProductItem title="watch" price="100"/>
+      <ProductItem title="watch" price="100" features={options[0]}/>
       <ProductItem title="mobile" price="200"/>
       <ProductItem title="laptop" price="300"/>
       <ProductItem title="TV" price="400"/>
