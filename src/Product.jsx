e394c1e14cddb9a61676d90346a99f99d73046c8
Abhay9326397439 <abhayzombade26@gmail.com>
2026-01-01 16:10:33 +0530

    render array's as props

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,13 +1,13 @@
 import ProductItem from "./ProductItem";
 
 function Product(){
-    let options = [{color:"red"}, {size:"M"}, {brand:"xyz"}];
+    let options = ["high_quality", " long_battery", " value_for_money", " powerful"];
     return (
         <div>
       <ProductItem title="watch" price="100" features={options[0]}/>
-      <ProductItem title="mobile" price="200"/>
-      <ProductItem title="laptop" price="300"/>
-      <ProductItem title="TV" price="400"/>
+      <ProductItem title="mobile" price="200"features={options[1]}/>
+      <ProductItem title="laptop" price="300" features={options[2]}/>
+      <ProductItem title="TV" price="400" features={options[3]}/>
      </div>
     );
 }
--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -4,7 +4,7 @@ function ProductItem({title, price, features}) {
     return (
         <div>
         <h2 className="product-title">This is a {title} and this is that product price is={price}</h2>
-        <h3 className="product-features">Features: {features.color}</h3>
+        <h3 className="product-features">Features: {features}</h3>
         </div>
     );
 
