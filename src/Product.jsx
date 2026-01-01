125e173a8709fe9ed9f8209b575a1bf1b7be95d5
Abhay9326397439 <abhayzombade26@gmail.com>
2026-01-01 16:54:30 +0530

    using conditioning statement

--- src/Product.jsx
+++ src/Product.jsx
@@ -4,10 +4,10 @@ function Product(){
     let options = ["high_quality", " long_battery", " value_for_money", " powerful"];
     return (
         <div>
-      <ProductItem title="watch" price="100" features={options[0]}/>
-      <ProductItem title="mobile" price="200"features={options[1]}/>
-      <ProductItem title="laptop" price="300" features={options[2]}/>
-      <ProductItem title="TV" price="400" features={options[3]}/>
+      <ProductItem title="watch" price={5000} />
+      <ProductItem title="mobile" price={3000}/>
+      <ProductItem title="laptop" price={1000}/>
+      <ProductItem title="TV" price={2000}/>
      </div>
     );
 }
--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,13 +1,14 @@
 import "./ProductItem.css";
 
-function ProductItem({title, price, features}) {
+function ProductItem({title, price}) {
+    let isdiscount= price>=3000 ? "You are eligible for a discount on this product!" : "you are not eligible for discount";
     return (
         <div>
         <h2 className="product-title">This is a {title} and this is that product price is={price}</h2>
-        <h3 className="product-features">Features: {features}</h3>
+        <p className="discount-info">{isdiscount}</p>
         </div>
     );
-
+   
 }
 
 export default ProductItem;
