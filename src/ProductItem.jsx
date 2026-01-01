935542d2637edeb90d714e7fd65bd6016a588574
Abhay9326397439 <abhayzombade26@gmail.com>
2026-01-01 17:22:08 +0530

    using ternary operator

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,14 +1,14 @@
 import "./ProductItem.css";
 
-function ProductItem({title, price}) {
-    let isdiscount= price>=3000 ? "You are eligible for a discount on this product!" : "you are not eligible for discount";
+function ProductItem({ title, price }) {
     return (
         <div>
-        <h2 className="product-title">This is a {title} and this is that product price is={price}</h2>
-        <p className="discount-info">{isdiscount}</p>
+            <h2 className="product-title">
+                This is a {title} and product price is {price}
+            </h2>
+            {price >=3000 ? (<p> profit you 'll get: {price *0.30} </p>) : null}
         </div>
     );
-   
 }
 
 export default ProductItem;
