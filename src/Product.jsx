aa45b9331a4ec6fc280cf461fe6b90ff4d89f967
Abhay9326397439 <abhayzombade26@gmail.com>
2026-01-03 23:57:00 +0530

    using props with dynamic styling

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,9 +1,12 @@
 import ProductItem from "./ProductItem";
+import User from "./User";
 
 function Product(){
     let options = ["high_quality", " long_battery", " value_for_money", " powerful"];
     return (
         <div>
+       <User name="Abhay" textColor="blue"/>
+       <User name="Manisha" backGroundColor="yellow"/>
       <ProductItem title="watch" price={5000} />
       <ProductItem title="mobile" price={3000}/>
       <ProductItem title="laptop" price={1000}/>
