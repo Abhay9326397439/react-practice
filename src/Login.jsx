49114de46f875fbe2d7542a72618b21f6ed21183
Abhay9326397439 <abhayzombade26@gmail.com>
2026-01-04 23:05:40 +0530

    add onclick even on button

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,17 @@
+
+function Display(){
+    return (
+            alert("login successful")
+    );
+}
+
 function Login(){
     return (
         <div>
             <form name="login">
                 Username:<input type="text" placeholder="Enter username"></input><br></br>
                 Password:<input type="password" placeholder="Enter password"></input><br></br>
-                <button>Login here</button>
+                <button onclick={Display()}>Login here</button>
             </form>
         </div>
     );
