8ae933f2b3f5ffbc1811069cf041a98d25d19a02
Abhay9326397439 <abhayzombade26@gmail.com>
2026-01-05 16:05:31 +0530

    add product card-wise

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,15 +2,18 @@ import ProductItem from "./ProductItem";
 import User from "./User";
 
 function Product(){
-    let options = ["high_quality", " long_battery", " value_for_money", " powerful"];
+    let styles={
+        display: "flex",
+        flexWrap:"wrap",
+         gap: "50px",
+    };
+    let option= ["Rs-1000", "Rs-2000", "Rs-3000", "Rs-4000"];
     return (
-        <div>
-       <User name="Abhay" textColor="blue"/>
-       <User name="harshad" backGroundColor="yellow"/>
-      <ProductItem title="watch" price={5000} />
-      <ProductItem title="mobile" price={3000}/>
-      <ProductItem title="laptop" price={1000}/>
-      <ProductItem title="TV" price={2000}/>
+        <div style={styles}>
+      <ProductItem title="watch"  Description="high quality" Price={option[0]}/>
+      <ProductItem title="mobile" Description="realible" Price={option[1]}/>
+      <ProductItem title="laptop" Description="powerful" Price={option[2]}/>
+      <ProductItem title="TV" Description="value for money" Price={option[3]} />
      </div>
     );
 }
