406354d39feb5a84c050824a8e56b20d22e2ade5
Abhay9326397439 <abhayzombade26@gmail.com>
2026-01-07 12:45:35 +0530

    add conter as per click on icon

--- Heart.jsx
+++ Heart.jsx
@@ -2,18 +2,22 @@ import { useState } from "react";
 
 export default function Heart(){
     let [liked,setLiked]=useState(false);
+    let [count,setCount]=useState(0);
     let Styles={
         fontSize:"40px",
+        color:"red",
     }
     let toggle=()=>{
         setLiked(!liked);
+        setCount(count+1);
     }
     return (
         <div>
             <h1 style={{ textAlign: "center", color: "red", fontFamily: "Times New Roman", fontStyle: "italic" }}>This is a Heart Component Using State</h1><br></br>
             <p onClick={toggle}>
+                <p style={{fontSize:"40px",color:"navy"}}>Counter={count}</p>
                 { liked ?
-                <i class="fa-solid fa-heart" style={Styles}></i> :
+                <i className="fa-solid fa-heart" style={Styles}></i> :
                 <i className="fa-regular fa-heart" style={Styles}></i>
                 }
             </p>
